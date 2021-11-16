d151acc9e8b328ff7cb3803e7f82d285b0e4b549
oxb4f <off0xbffff@gmail.com>
2021-11-16 21:44:02 +0200

    Minor improvements

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sort -u -t '=' -k 1,1 env/"$1"/.env env/local/.env | grep -v '^$\|^\s*\#' > env/.env
+sort -u -t '=' -k 1,1 env/local/.env env/"$1"/.env | grep -v '^$\|^\s*\#' > env/.env
 
 docker-compose --env-file env/.env -f docker/"$1"/docker-compose.yaml -p api up --build -d
