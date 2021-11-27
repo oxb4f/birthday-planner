4f5250c66f2b0e57d4ab8b1069738ddce38d2340
oxb4f <off0xbffff@gmail.com>
2021-11-27 22:21:35 +0200

    Update config

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -4,4 +4,4 @@ _ENV=$1
 
 sort -u -t '=' -k 1,1 env/local/.env env/"$_ENV"/.env | grep -v '^$\|^\s*\#' > env/.env
 
-NODE_ENV=$_ENV docker-compose --env-file env/.env -f docker/"$_ENV"/docker-compose.yaml -p api up --build -d
+docker-compose --env-file env/.env -f docker/"$_ENV"/docker-compose.yaml -p api up --build -d
