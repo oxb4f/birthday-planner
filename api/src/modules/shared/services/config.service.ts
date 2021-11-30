64a2de62afe10f4701103f44eaf580a1b9a4a659
oxb4f <off0xbffff@gmail.com>
2021-11-30 22:32:12 +0200

    Updated conifg

--- api/src/modules/shared/services/config.service.ts
+++ api/src/modules/shared/services/config.service.ts
@@ -10,10 +10,11 @@ export class ConfigService {
 
   constructor() {
     this._nodeEnv = process.env["NODE_ENV"] ?? "dev";
+
+    const envPath = this._nodeEnv === "dev" ? "env/.env" : ".env";
+
     this._envConfig = parseEnvFile(
-      readFileSync(
-        join(process.cwd(), "env/.env"),
-      ),
+      readFileSync(join(process.cwd(), envPath)),
     );
   }
 
