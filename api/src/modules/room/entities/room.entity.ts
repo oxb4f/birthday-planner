d0dc812ecdbfd0fbd30e97a10ad9f278c2c44b46
oxb4f <off0xbffff@gmail.com>
2021-12-14 22:15:35 +0200

    Added entities for room module

--- api/src/modules/room/entities/room.entity.ts
+++ api/src/modules/room/entities/room.entity.ts
@@ -1,6 +1,17 @@
-import { Entity, Index, ManyToOne, Property } from "@mikro-orm/core";
+import {
+  Entity,
+  Index,
+  ManyToOne,
+  Property,
+  OneToMany,
+  Collection,
+  OneToOne,
+} from "@mikro-orm/core";
+
 import { User } from "../../user/entities";
 import { BaseEntity } from "../../shared/entities";
+import { RoomParticipant } from "./room-participant.entity";
+import { RoomInvite } from "./room-invite.entity";
 
 @Entity()
 export class Room extends BaseEntity {
@@ -10,6 +21,12 @@ export class Room extends BaseEntity {
   @Index({ name: "room_user_id_index" })
   @ManyToOne()
   public readonly owner!: User;
+
+  @OneToMany(() => RoomParticipant, (roomParticipant) => roomParticipant.room)
+  public readonly participants = new Collection<RoomParticipant>(this);
+
+  @OneToOne()
+  public readonly invite!: RoomInvite;
 
   constructor(title: string, owner: User) {
     super();
